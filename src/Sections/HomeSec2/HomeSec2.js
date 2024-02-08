3939856e5153200ca6bc384a4af3106dd6de8dd2
lakshendra02 <sejwanilakshen@gmail.com>
2024-02-08 23:16:31 +0530

    quote and Section images in HomeSec2 HomeSec3 Home

--- src/Sections/HomeSec2/HomeSec2.js
+++ src/Sections/HomeSec2/HomeSec2.js
@@ -1,17 +1,17 @@
 import React from "react";
 import "./HomeSec2.css";
 
-const HomeSec2 = () => {
+const HomeSec2 = ({SecImg}) => {
   return (
     <div className="home-sec-2-main-container">
       <div className="home-sec-2-text-container">
         <h1>Experience the thrill of sports</h1>
         <p>
-          Discover the biggest and best sports festivals from around the world.
+        It's not whether you get knocked down; it's whether you get up.
         </p>
       </div>
       <img
-        src="https://images.pexels.com/photos/163444/sport-treadmill-tor-route-163444.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+      src={SecImg}
         alt="sports"
       />
     </div>
--- src/Sections/HomeSec3/HomeSec3.js
+++ src/Sections/HomeSec3/HomeSec3.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./HomeSec3.css";
 
-const HomeSec3 = () => {
+const HomeSec3 = ({SecImg}) => {
   return (
     <div className="home-sec-2-main-container sec-3-main-container">
       <img
         className="sec-3-img"
-        src="https://images.pexels.com/photos/163444/sport-treadmill-tor-route-163444.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+        src={SecImg}
         alt="sports"
       />
       <div className="home-sec-2-text-container">
