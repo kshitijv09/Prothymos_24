844799104101b23bcd4984ed0a5cb09e578473a9
Tanyajha-2003 <tanyakumar2003@gmail.com>
2024-02-10 22:48:50 +0530

    Added SportCouncil card,rendered council

---
+++ src/Components/CouncilCard/CouncilCardData.js
@@ -0,0 +1,17 @@
+// import Anish from "../../Assets/anish.jpeg"
+
+const CouncilCardData = [
+    {
+        name: "Himanshu Reddy",
+        position:"Sports Secretary",
+        img: "",
+    },
+    {
+        name: "Rohit S Bellur",
+        position:"Vice Sports Secretary",
+        // img: Anish,
+    },
+
+]
+
+export default CouncilCardData;
