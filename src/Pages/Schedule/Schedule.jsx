ed05d95fb8008834c9d696c86628fd8871936d44
kshitijv09 <kshitijv09@gmail.com>
2024-02-15 12:03:13 +0530

    Added Results Page, Integrated Coming Soon Component

--- src/Pages/Schedule/Schedule.jsx
+++ src/Pages/Schedule/Schedule.jsx
@@ -4,53 +4,62 @@ import Footer from '../../Sections/Footer/Footer';
 import ScheduleCard from '../../Components/ScheduleCard/ScheduleCard';
 import { useParams } from 'react-router-dom';
 import cricket_schedule from '../../Data/Schedule/CricketSchedule';
+import ComingSoonComp from '../../Components/Coming_soon/Coming_soon';
 import './Schedule.css';
 
 export default function Schedule() {
     const [sportSchedule, setSportSchedule] = useState([]);
-    const {sport}=useParams()
+    const { sport } = useParams();
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         // Fetch data based on the sport name
         switch (sport) {
-          case "Cricket":
-            setSportSchedule(cricket_schedule);
-            break;
-          case "Volleyball":
-            setSportSchedule(cricket_schedule);
-            break;
-          case "Throwball":
-            setSportSchedule(cricket_schedule);
-            break;
-          case "Kabaddi":
-            setSportSchedule(cricket_schedule);
-            break;
-          case "Football":
-            setSportSchedule(cricket_schedule);
-            break;
-          default:
-            setSportSchedule([]);
-            break;
+            case "Cricket":
+                setSportSchedule(cricket_schedule);
+                break;
+            case "Volleyball":
+                setSportSchedule(cricket_schedule);
+                break;
+            case "Throwball":
+                setSportSchedule(cricket_schedule);
+                break;
+            case "Kabaddi":
+                setSportSchedule(cricket_schedule);
+                break;
+            case "Football":
+                setSportSchedule(cricket_schedule);
+                break;
+            default:
+                setSportSchedule([]);
+                break;
         }
-      }, [sport]);
+    }, [sport]);
+
+    useEffect(() => {
+        if (sportSchedule.length > 0) {
+            setIsLoading(false);
+        }
+    }, [sportSchedule]);
 
     return (
         <div>
-            <Navbar/>
-            <div className='outer-schedule-cont'>
-            <div>
-
-            
-            <h1 className='schedule-title'>{sport} Schedule</h1>
-            </div>
-            <div className='sport-schedule-container'>
-            {sportSchedule.map((mp, index) => (
-                <ScheduleCard key={index} schedule={mp} />
-            ))}
-            </div>
-            </div>
-            
-            <Footer/>
+            <Navbar />
+            {isLoading ? (
+                <ComingSoonComp />
+            ) : (
+                <div className='outer-schedule-cont'>
+                    <div>
+                        <h1 className='schedule-title'>{sport} Schedule</h1>
+                    </div>
+                    <div className='sport-schedule-container'>
+                        {sportSchedule.map((mp, index) => (
+                            <ScheduleCard key={index} schedule={mp} />
+                        ))}
+                    </div>
+                </div>
+            )}
+            <Footer />
         </div>
     );
 }
