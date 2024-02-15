d13a460459ee51d82a201e633f98586df9f71154
Shashi-Preetam1 <shashipreetam00@gmail.com>
2024-02-15 23:18:46 +0530

    Updated cricket schedule.

--- src/Data/Schedule/CricketSchedule.js
+++ src/Data/Schedule/CricketSchedule.js
@@ -1,52 +1,109 @@
- const cricket_schedule=[
-    {
-       team1:"Team 1",
-       team2:"Team 2",
-       venue:" MainGround",
-       day:"Day 1",
-       time:"7:00 A.M."
-    },
-    {
-        team1:"Team 1",
-        team2:"Team 2",
-        venue:" MainGround",
-        day:"Day 1",
-        time:"7:00 A.M."
-     },
-     {
-        team1:"Team 1",
-        team2:"Team 2",
-        venue:" MainGround",
-        day:"Day 1",
-        time:"7:00 A.M."
-     },
-     {
-        team1:"Team 1",
-        team2:"Team 2",
-        venue:" MainGround",
-        day:"Day 1",
-        time:"7:00 A.M."
-     },
-     {
-        team1:"Team 1",
-        team2:"Team 2",
-        venue:" MainGround",
-        day:"Day 1",
-        time:"7:00 A.M."
-     },
-     {
-        team1:"Team 1",
-        team2:"Team 2",
-        venue:" MainGround",
-        day:"Day 1",
-        time:"7:00 A.M."
-     },
-     {
-        team1:"Team 1",
-        team2:"Team 2",
-        venue:" MainGround",
-        day:"Day 1",
-        time:"7:00 A.M."
-     },
-]
+const cricket_schedule = [
+   {
+     team1: "Team Skeet",
+     team2: "Speed Breakers",
+     venue: "Amity ground",
+     day: "Day 1",
+     time: "7:00 A.M."
+   },
+   {
+     team1: "Akash Vani",
+     team2: "Fury X1",
+     venue: "Amity ground",
+     day: "Day 1",
+     time: "10:00 A.M."
+   },
+   {
+     team1: "Moye Moye",
+     team2: "Royal Challengers Tiruchhirappali",
+     venue: "Practice ground",
+     day: "Day 1",
+     time: "10:00 A.M."
+   },
+   {
+     team1: "Sunrisers",
+     team2: "Rule Breakers",
+     venue: "Practice ground",
+     day: "Day 1",
+     time: "12:00 P.M."
+   },
+   {
+     team1: "Team Skeet",
+     team2: "Moye Moye",
+     venue: "Amity ground",
+     day: "Day 1",
+     time: "4:00 P.M."
+   },
+   {
+     team1: "Speed Breakers",
+     team2: "Royal Challengers Tiruchirappalli",
+     venue: "Practice ground",
+     day: "Day 1",
+     time: "3:00 P.M."
+   },
+   {
+     team1: "Akash Vani",
+     team2: "Rule Breakers",
+     venue: "Amity ground",
+     day: "Day 2",
+     time: "7:00 A.M."
+   },
+   {
+     team1: "Team Skeet",
+     team2: "Royal Challengers Tiruchirappalli",
+     venue: "Practice ground",
+     day: "Day 2",
+     time: "9:00 A.M."
+   },
+   {
+     team1: "Speed Breakers",
+     team2: "Moye Moye",
+     venue: "Amity ground",
+     day: "Day 2",
+     time: "9:30 A.M."
+   },
+   {
+     team1: "Fury X1",
+     team2: "Sunrisers",
+     venue: "Practice ground",
+     day: "Day 2",
+     time: "11:30 A.M."
+   },
+   {
+     team1: "Akash Vani",
+     team2: "Sunrisers",
+     venue: "Amity ground",
+     day: "Day 2",
+     time: "4:00 P.M."
+   },
+   {
+     team1: "Fury X1",
+     team2: "Rule Breakers",
+     venue: "Practice ground",
+     day: "Day 2",
+     time: "3:00 P.M."
+   },
+   {
+     team1: "Pool A 1st Place",
+     team2: "Pool B 2nd Place",
+     venue: "Practice ground",
+     day: "Day 3",
+     time: "9:00 A.M."
+   },
+   {
+     team1: "Pool A 2nd Place",
+     team2: "Pool B 1st Place",
+     venue: "Amity ground",
+     day: "Day 3",
+     time: "9:00 A.M."
+   },
+   {
+      team1: "Finalist 1",
+      team2: "Finalist 2",
+      venue: "Amity ground",
+      day: "Day 3",
+      time: "4:00 P.M."
+    }
+ ];
+ 
 export default cricket_schedule;
