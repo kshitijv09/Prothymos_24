ef9796e8470dc6d1c8bc216ce6498db2d9404879
Tanyajha-2003 <tanyakumar2003@gmail.com>
2024-02-18 09:46:21 +0530

    Data update

--- src/Data/Result/CricketResult.js
+++ src/Data/Result/CricketResult.js
@@ -1,26 +1,75 @@
 const cricket_result_1 = [
     {
       match_no:1,
-      team1: "Team A",
-      team2: "Team d",
-      result: "Teamfjf kfts"
+      team1: "Team Skeet",
+      team2: "Team Speed Breakers",
+      result: "Team Speed Breakers won"
     },
     {
         match_no:2,
-      team1: "Team C",
-      team2: "Team D",
-      result: "Match abandoned due to rain"
+      team1: "Team Moye Moye",
+      team2: "Team Royal Challengers Tiruchhirappali",
+      result: "Team Moye Moye won"
     },
     {
         match_no:3,
-        team1: "Team C",
-        team2: "Team D",
-        result: "Match abandoned due to rain"
+        team1: "Team Akash Vani",
+        team2: "Team Fury X1",
+        result: "Team Fury X1 won"
       },
+      {
+        match_no:4,
+        team1: "Team RCT",
+        team2: "Team Speed Breakers",
+        result: "Team RCT won"
+      },
+      {
+        match_no:5,
+        team1: "Team Skeet",
+        team2: "Team Moye Moye",
+        result: "Team skeet won"
+      }
     
   ];
   const cricket_result_2 = [
-    
+    {
+      match_no:1,
+      team1: "Team Akash Vani",
+      team2: "Team Rule Breakers",
+      result: "Team Akash Vani won"
+    },
+    {
+      match_no:2,
+      team1: "Team RCT",
+      team2: "Team Skeet",
+      result: "Team RCT won"
+    },
+    {
+      match_no:3,
+      team1: "Team Moye Moye",
+      team2: "Team Speed Breakers",
+      result: "Team Moye Moye won"
+    },
+   
+    {
+      match_no:4,
+      team1: "Team Fury X1",
+      team2: "Team Sunrisers",
+      result: "Team Fury X1 won"
+    },
+    {
+      match_no:5,
+      team1: "Team Rule Breakers",
+      team2: "Team Furi X1",
+      result: "Team Fury X1 won"
+    },
+    {
+      match_no:6,
+      team1: "Team Akashvani",
+      team2: "Team Sunrisers",
+      result: "Team Akashvani won"
+    },
+  
   ];
   const cricket_result_3 = [
     
